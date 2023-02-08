2ebfd19c6f09949e91a42887420c0259b8881703
Sherfin Shamsudeen <sherfin94@gmail.com>
2023-02-08 11:53:18 +0530

    Revert "[Bug]: Fixed app crashes when items dropped into container inside listview (#5449)"
    This reverts commit f6e23fbd65219a8792d49a06ac2a1930116a6a10.

--- frontend/src/Editor/Box.jsx
+++ frontend/src/Editor/Box.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo, useContext, useRef } from 'react';
+import React, { useEffect, useState, useMemo, useContext } from 'react';
 import { Button } from './Components/Button';
 import { Image } from './Components/Image';
 import { Text } from './Components/Text';
@@ -196,8 +196,6 @@ export const Box = function Box({
 
   const { variablesExposedForPreview, exposeToCodeHinter } = useContext(EditorContext) || {};
 
-  const componentActions = useRef(new Set());
-
   useEffect(() => {
     const currentPage = currentState?.page;
 
@@ -311,16 +309,11 @@ export const Box = function Box({
                 ) {
                   if (!Object.keys(exposedVariables).includes(actionName)) {
                     func.dependencies = dependencies;
-                    componentActions.current.add(actionName);
                     return onComponentOptionChanged(component, actionName, func);
                   } else if (exposedVariables[actionName]?.dependencies?.length === 0) {
                     return Promise.resolve();
-                  } else if (
-                    JSON.stringify(dependencies) !== JSON.stringify(exposedVariables[actionName]?.dependencies) ||
-                    !componentActions.current.has(actionName)
-                  ) {
+                  } else if (!_.isEqual(dependencies, exposedVariables[actionName]?.dependencies)) {
                     func.dependencies = dependencies;
-                    componentActions.current.add(actionName);
                     return onComponentOptionChanged(component, actionName, func);
                   }
                 }
