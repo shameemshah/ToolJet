258510e17bc943f7af99b7253296e13eca13aeef
stepinfwd <stepinfwd@gmail.com>
2023-04-03 10:44:13 +0530

    init mailgun

--- plugins/packages/mailgun/lib/index.ts
+++ plugins/packages/mailgun/lib/index.ts
@@ -1,35 +1,56 @@
 import { QueryError, QueryResult, QueryService } from '@tooljet-plugins/common';
 import { SourceOptions, QueryOptions, EmailOptions } from './types';
-import MailgunSdk from 'mailgun.js';
-import FormData from 'form-data';
+const FormData = require('form-data');
+import Mailgun from 'mailgun.js';
+const mailgun = new Mailgun(FormData);
 
-export default class Mailgun implements QueryService {
+export default class MailgunLib implements QueryService {
   async run(sourceOptions: SourceOptions, queryOptions: QueryOptions, dataSourceId: string): Promise<QueryResult> {
     if (!(queryOptions && sourceOptions.api_key)) {
       throw new QueryError('Query could not be completed as API key is not set', 'Missing API key', {});
     }
 
-    const sdk = new MailgunSdk(FormData);
-    const mailgunOptions = { username: 'api', key: sourceOptions.api_key, url: null };
+    // const sdk = new MailgunSdk(FormData);
+    // const mailgunOptions = { username: 'api', key: sourceOptions.api_key, url: null };
+    const mailgunOptions: any = mailgun.client({ username: 'api', key: sourceOptions.api_key });
+
     if (sourceOptions.eu_hosted) {
       mailgunOptions.url = 'https://api.eu.mailgun.net';
     }
-    const mailGunClient = sdk.client(mailgunOptions);
+    // const mailGunClient = sdk.client(mailgunOptions);
 
     let result = {};
-    const emailOptions: EmailOptions = {
-      to: queryOptions.send_mail_to,
-      from: queryOptions.send_mail_from,
-      subject: queryOptions.subject,
-      text: queryOptions.text,
-    };
+    // const emailOptions: EmailOptions = {
+    //   to: queryOptions.send_mail_to,
+    //   from: queryOptions.send_mail_from,
+    //   subject: queryOptions.subject,
+    //   text: queryOptions.text,
+    // };
+    const emailOptions: EmailOptions =
+      // .create(sourceOptions.domain, {
+      {
+        // from: queryOptions.send_mail_from,
+        // to: queryOptions.send_mail_to,
+        // subject: queryOptions.subject,
+        // message: queryOptions.text,
+        from: 'Excited User <adish@tooljet.com>',
+        to: ['stepinfwd@gmail.com'],
+        subject: 'Hello',
+        text: 'Testing some Mailgun awesomness!',
+        // html: '<h1>Testing some Mailgun awesomness!</h1>',
+      };
+    // })
+    // .then((msg) => console.log(msg)) // logs response data
+    // .catch((err) => console.error(err)); // logs any error
 
     if (queryOptions.html && queryOptions.html.length > 0) {
       emailOptions.html = queryOptions.html;
     }
 
     try {
-      result = await mailGunClient.messages.create(sourceOptions.domain, emailOptions);
+      console.log('oprions', sourceOptions.domain, emailOptions);
+      result = await mailgunOptions.messages.create(sourceOptions.domain, emailOptions);
+      // mg.messages.create(domain, data)
     } catch (error) {
       console.error(error);
       throw new QueryError('Query could not be completed', error.message, {});
